3b1c0b9b8794bfaf4e09acd20329a57301d50464
Check-HazratALi <162535361+Check-HazratALi@users.noreply.github.com>
2024-03-21 14:19:03 +0600

    P

---
+++ 1stdigit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    if ((a/1000)%2==0)
+    {
+        cout<<"EVEN";
+    }
+    else
+    {
+        cout<<"ODD";
+    }
+    
+    
+    return 0;
+}
