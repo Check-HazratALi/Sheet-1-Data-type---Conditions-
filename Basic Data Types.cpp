a6917b0105d4502a6b4f861802642b67b86c9423
Check-HazratALi <162535361+Check-HazratALi@users.noreply.github.com>
2024-03-21 14:11:07 +0600

    B

---
+++ Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    long long a;
+    char c;
+    float f;
+    double d;
+
+    cin>>i>>a>>c>>f>>d;
+    cout<<i<<"\n"<<a<<"\n"<<c<<"\n"<<f<<"\n"<<d;
+    return 0;
+}
