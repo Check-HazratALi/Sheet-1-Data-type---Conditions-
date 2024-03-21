2de7441a654c52f178892d70a87394eb1d2edf31
Check-HazratALi <162535361+Check-HazratALi@users.noreply.github.com>
2024-03-21 14:11:23 +0600

    C

---
+++ Simple Calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    long long a,b,sum,multiple,minus;
+    cin>>a>>b;
+    cout<<a<<" + "<<b<<" = "<<a+b<<"\n";
+    cout<<a<<" * "<<b<<" = "<<a*b<<"\n";
+    cout<<a<<" - "<<b<<" = "<<a-b;
+    cout << a * b;
+    return 0;
+}
