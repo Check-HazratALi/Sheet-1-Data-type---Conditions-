5d9c814741e126dd303883f63cd47a37aa631557
Check-HazratALi <162535361+Check-HazratALi@users.noreply.github.com>
2024-03-21 14:12:44 +0600

    G

---
+++ Summation from 1 to N.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    
+    cout << "Enter a number : "; 
+    cin >> n;
+    
+    cout << n*(n+1)/2;
+    
+    return 0;
+}
